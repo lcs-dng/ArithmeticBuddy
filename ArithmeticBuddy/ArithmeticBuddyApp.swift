b060860a5807bbdabb1130d49fa31e3d073b13e0
Dexter Ng <DNg@lcs.on.ca>
2024-02-03 17:10:43 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by Dexter Ng on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
